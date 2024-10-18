3e24c8837945dafc5336f2b7e72d4ef050a84f1d
Diego Fernando Guerrero Perez <diego.guerrero9612@gmail.com>
2024-10-18 15:23:45 -0500

    Primer commit se crea el proyecto y se agrega la configuracion en properties para conexion a la base de datos

---
+++ src/main/java/com/diego/springboot/app/crud/project_springboot_jpa_crud_app/ProjectSpringbootJpaCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.diego.springboot.app.crud.project_springboot_jpa_crud_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringbootJpaCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringbootJpaCrudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diego/springboot/app/crud/project_springboot_jpa_crud_app/ProjectSpringbootJpaCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.springboot.app.crud.project_springboot_jpa_crud_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringbootJpaCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
